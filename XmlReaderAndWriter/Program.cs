189023c05d26233f9de6cdb0b1c9db8475be0594
AfraAllahyani <Afra.Allahyani@tuwaiq.edu.sa>
2021-07-01 16:30:18 +0300

    Create XML File Contains Team Members Info

--- XmlReaderAndWriter/Program.cs
+++ XmlReaderAndWriter/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Xml;
 
 namespace XmlReaderAndWriter
 {
 	class Program
 	{
-		static void Main(string[] args)
+        static void CreateXMLFileContainsTeamMembersInfo()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+
+            XmlElement root = xmldoc.CreateElement("team");
+            xmldoc.AppendChild(root);
+
+            XmlElement element1 = xmldoc.CreateElement("member");
+            root.AppendChild(element1);
+
+            XmlElement element2 = xmldoc.CreateElement("id");
+            XmlText text2 = xmldoc.CreateTextNode("");
+            element1.AppendChild(element2).AppendChild(text2);
+          
+
+            XmlElement element3 = xmldoc.CreateElement("name");
+            XmlText text3 = xmldoc.CreateTextNode("");
+            element1.AppendChild(element3).AppendChild(text3);
+
+
+            XmlElement element4 = xmldoc.CreateElement("major");
+            XmlText text4 = xmldoc.CreateTextNode("");
+            element1.AppendChild(element4).AppendChild(text4);
+
+            XmlElement element5 = xmldoc.CreateElement("email");
+            XmlText text5 = xmldoc.CreateTextNode("");
+            element1.AppendChild(element5).AppendChild(text5);
+            
+            
+            xmldoc.Save(@"/XmlReaderAndWriter/Project5.xml");
+            Console.WriteLine(xmldoc.InnerXml);
+        }
+        static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
+
+            CreateXMLFileContainsTeamMembersInfo();
+            
+        }
 	}
 }
